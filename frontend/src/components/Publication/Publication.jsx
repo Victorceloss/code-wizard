e3fd5b94852ed967809419c7a5bd0ed049b8d9f8
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-05-06 17:18:06 +0100

    :sparkles: feat: Adding new elements to Publication.jsx

--- frontend/src/components/Publication/Publication.jsx
+++ frontend/src/components/Publication/Publication.jsx
@@ -1,5 +1,11 @@
-export const Publication = () => {
-  return (
+export const Publication = (publication) => {
+  const dataPublication = publication.publicationData;
+  console.log(dataPublication);
 
+  return (
+    <div className="main-publication">
+      <h1 className="publication-title"></h1>
+      <h2 className="publictation-autor"></h2>
+    </div>
   );
 };
