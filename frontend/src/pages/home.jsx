973611c51cbcd08a70480574bf2e9d0db7892a8d
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-05-06 14:54:06 +0100

    :sparkles: feat: Adding new elements to home.jsx

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -6,6 +6,25 @@ const Home = () => {
   return (
     <>
     <Header />
+    <main className="main">
+      <section className="section-info">
+        <h2 className="info-title">Programming Simplified</h2>
+        <h3 className="info-subtitle">Tutorials, Tips, and Tricks for All Levels</h3>
+        <p className='info-text'>
+          The world of programming can seem intimidating, but it doesn&apos;t 
+          have to be. Here, we offer tutorials, tips, and tricks for all levels 
+          of programming, from beginners to experienced programmers. Our resources 
+          are designed to make programming simpler and more accessible to everyone.
+        </p>
+        <div className="info-buttons">
+          <button className="buttons-started">Get Started</button>
+        </div>
+      </section>
+      <section className="section-image">
+        <img src="" alt="" className="image-content" />
+        <div className='square-layout'></div>
+      </section>
+    </main>
     <Footer />
     </>
   );
