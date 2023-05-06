e0407263741f060873f40ca585d25392de94e679
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-05-06 13:19:04 +0100

    :sparkles: feat: Adding new elements to Header.jsx

--- frontend/src/components/Header/header.jsx
+++ frontend/src/components/Header/header.jsx
@@ -1,9 +1,23 @@
 import "./header.css";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <header className="header">
-
-    </header>
+    <div className="header-component">
+      <header className="header">
+        <h1 className="header-title">
+          <i className="fa-solid fa-hat-wizard"></i> CodeWizard
+        </h1>
+        <nav className="header-nav">
+          <Link className="nav-link" to={'/'}>Home</Link>
+          <Link className="nav-link" to={'/'}>Blog</Link>
+          <Link className="nav-link" to={'/'}>About</Link>
+        </nav>
+        <div className="header-buttons">
+          <button className="buttons-login">Sign In</button>
+          <button className="buttons-register">Sign Up</button>
+        </div>
+      </header>
+    </div>
   );
 };
