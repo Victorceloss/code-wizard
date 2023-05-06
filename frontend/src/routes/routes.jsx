d934365068b05953c2ecfa46ad1854d953fa560a
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-05-06 16:27:00 +0100

    :sparkles: feat: Adding a route to the "Blog" page

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "../pages/home";
+import Blog from "../pages/blog";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={ <Home/> } />
+        <Route path="/blog" element={ <Blog /> } />
       </Routes>
     </BrowserRouter>
   );
