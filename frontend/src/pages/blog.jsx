06bb90baaa9e363241a6f20abaafd41d3f345997
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-05-06 16:28:30 +0100

    :sparkles: feat: Imported Header and Footer component

--- frontend/src/pages/blog.jsx
+++ frontend/src/pages/blog.jsx
@@ -1,7 +1,14 @@
+import { Header } from "../components/Header/header";
+import { Footer } from "../components/Footer/footer";
+
 const Blog = () => {
   return (
     <>
-    
+    <Header />
+    <main className="blog-main">
+      
+    </main>
+    <Footer />
     </>
   );
 };
