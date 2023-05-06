254b2e225cebccbc0381071772a834dc6316db26
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-05-06 17:05:45 +0100

    :sparkles: feat: Add listAllContent function

---
+++ frontend/src/services/TabnewsAPI.jsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const baseURL = `https://www.tabnews.com.br/api/v1`;
+
+export const listAllContent = async (page) => {
+  try {
+    const axiosResponse = await axios.get(`${baseURL}/contents?page=${page}&per_page=10&strategy=relevant`);
+    return axiosResponse.data;
+  } catch (error) {
+    console.log(`An error occurred in the request: ${error}`);
+
+    if (error.response && error.response.status === 429)
+    {
+      console.log("Retrying after 1 second");
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      return listAllContent(page);
+    }
+  }
+}
