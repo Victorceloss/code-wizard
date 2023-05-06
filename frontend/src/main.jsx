88ca7ed59777f41e5642182b4b34513324272b84
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-05-06 12:55:13 +0100

    :sparkles: feat: Add style file and import it into the main file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import { Router } from './routes/routes';
 
+import "./styles/index.css";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router />
 )
