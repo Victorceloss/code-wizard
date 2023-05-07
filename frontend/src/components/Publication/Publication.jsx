e4d3179cc167187597e092c795b4f39617b58950
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-05-07 11:01:26 +0100

    :sparkles: feat: Adding the Link to the Post Content

--- frontend/src/components/Publication/Publication.jsx
+++ frontend/src/components/Publication/Publication.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./Publication.css";
 
 export const Publication = (publication) => {
@@ -5,9 +6,11 @@ export const Publication = (publication) => {
   console.log(dataPublication);
 
   return (
-    <div className="main-publication">
-      <h1 className="publication-title">{dataPublication.title}</h1>
-      <h2 className="publictation-autor">Created By {dataPublication.owner_username}</h2>
-    </div>
+    <Link className="publication-link" to={`/${dataPublication.owner_username}/${dataPublication.slug}`}>
+      <div className="main-publication">
+        <h1 className="publication-title">{dataPublication.title}</h1>
+        <h2 className="publictation-autor">Created By {dataPublication.owner_username}</h2>
+      </div>
+    </Link> 
   );
 };
