3f40116af145581fe1e155c44c4393a8f219d253
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-05-07 12:14:49 +0100

    :bug: fix: Correcting publication link

--- frontend/src/components/Publication/Publication.jsx
+++ frontend/src/components/Publication/Publication.jsx
@@ -6,7 +6,7 @@ export const Publication = (publication) => {
   console.log(dataPublication);
 
   return (
-    <Link className="publication-link" to={`/${dataPublication.owner_username}/${dataPublication.slug}`}>
+    <Link target="_blank" className="publication-link" to={`https://www.tabnews.com.br/${dataPublication.owner_username}/${dataPublication.slug}`}>
       <div className="main-publication">
         <h1 className="publication-title">{dataPublication.title}</h1>
         <h2 className="publictation-autor">Created By {dataPublication.owner_username}</h2>
