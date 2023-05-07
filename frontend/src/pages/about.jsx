42f8c3fbeb40507bec2ab9e46185cf372fd002f7
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-05-07 18:29:20 +0100

    :sparkles: feat: Imported Header and Footer component

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -1,9 +1,14 @@
+import { Header } from "../components/Header/header";
+import { Footer } from "../components/Footer/footer";
+
 const About = () => {
   return (
     <>
+    <Header />
     <main className="about-main">
-      
+
     </main>
+    <Footer />
     </>
   )
 };
