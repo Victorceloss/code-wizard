88de2997eb816cf0e49347e8d571262964ed0603
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-05-07 11:11:53 +0100

    :sparkles: feat: Create API call to get post content

--- frontend/src/pages/postContent.jsx
+++ frontend/src/pages/postContent.jsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { showPostContent } from "../services/TabnewsAPI";
 
 import { Header } from "../components/Header/header";
 import { Footer } from "../components/Footer/footer";
 
 const Post = () => {
   const { name, slug } = useParams();
+  const [postContent, setPostContent] = useState([]);
+
+  const getPostContent = async () => {
+    const contentData = await showPostContent(name, slug);
+    setPostContent(contentData);
+  }
+
+  useEffect(() => {
+    getPostContent();
+  }, []);
+
+  console.log(postContent);
 
   return (
     <>
