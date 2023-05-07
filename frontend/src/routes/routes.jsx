af2e83066b8706f9d468208cee882c3c122f723d
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-05-07 11:00:38 +0100

    :sparkles: feat: Adding a route to the "Post" page

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "../pages/home";
 import Blog from "../pages/blog";
+import Post from "../pages/postContent";
 
 export const Router = () => {
   return (
@@ -9,6 +10,7 @@ export const Router = () => {
       <Routes>
         <Route path="/" element={ <Home/> } />
         <Route path="/blog" element={ <Blog /> } />
+        <Route path="/:name/:slug" element={ <Post /> } />
       </Routes>
     </BrowserRouter>
   );
