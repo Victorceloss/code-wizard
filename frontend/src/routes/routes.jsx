4f89a3d1dd07ae5c5bbce0c96441214f07a825bb
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-05-07 12:12:43 +0100

    :boom: feat: Remove the Post page from the project

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "../pages/home";
 import Blog from "../pages/blog";
-import Post from "../pages/postContent";
 
 export const Router = () => {
   return (
@@ -10,7 +9,6 @@ export const Router = () => {
       <Routes>
         <Route path="/" element={ <Home/> } />
         <Route path="/blog" element={ <Blog /> } />
-        <Route path="/:name/:slug" element={ <Post /> } />
       </Routes>
     </BrowserRouter>
   );
