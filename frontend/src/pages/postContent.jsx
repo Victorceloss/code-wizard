de21a248b0249b316cda1874a86e0029d52e6500
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-05-07 11:06:13 +0100

    :sparkles: feat: Add destructuring of 'useParams()' to get 'name' and 'slug'

--- frontend/src/pages/postContent.jsx
+++ frontend/src/pages/postContent.jsx
@@ -1,12 +1,16 @@
+import { useParams } from "react-router-dom";
+
 import { Header } from "../components/Header/header";
 import { Footer } from "../components/Footer/footer";
 
 const Post = () => {
+  const { name, slug } = useParams();
+
   return (
     <>
     <Header />
     <main className="main-post">
-      
+
     </main>
     <Footer />
     </>
